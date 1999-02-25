c214dc305ac5d5927bc4fa6c0fd8e30942ea33b0
Bill King <ungod@neuromancy.net>
1999-02-25 13:54:24 +0000

    no message
    git-svn-id: http://svn.neuromancy.net/magick/trunk/Magick-II@90 1d68a2a4-f605-0410-a2a0-863aa388aa54

--- src/chanserv.cpp
+++ src/chanserv.cpp
@@ -1,17 +1,22 @@
 #include "chanserv.h"
 #include "magick.h"
-#include <ace/Thread.h>
 
 // ***************************************
 // NOTE:this function has to be re-entrant
 // ***************************************
 void *chanserv_thread_handler(void *level)
 {
+    int ilevel=(int)level;  // 0 get's passed in for the first thread spawned, etc.
     MagickObject->ThreadtoTypeMap[ACE_Thread::self()]=tt_ChanServ;
     while(false) // fix this a bit later to a proper check
     {
+
 	// check for spawn limit hit
-	// if so ACE_Thread::spawn(chanserv_thread_handler,(void *)((int)level+1));
+	// if so ACE_Thread::spawn(chanserv_thread_handler,(void *)(ilevel+1));
+	ACE_Local_RLock *inputbufferlock=new ACE_Local_RLock("chanserv::inputbuffer");
+	// check the inputbuffer
+
+	delete inputbufferlock;
 	ACE_Thread::yield();
     }
     return NULL;
@@ -20,4 +25,11 @@ void *chanserv_thread_handler(void *level)
 void init_chanserv()
 {
     ACE_Thread::spawn(chanserv_thread_handler,(void *)0);
+}
+
+void ChanServ::push_message(const mstring& servicename, const mstring& message)
+{
+	ACE_Local_WLock *inputbufferlock=new ACE_Local_WLock("chanserv::inputbuffer");
+	inputbuffer.push_back(pair<mstring,mstring>(servicename,message));
+	delete inputbufferlock;
 }
