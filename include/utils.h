483d63d0b6d464093c8024a73ce090654f3a2276
Bill King <ungod@neuromancy.net>
1999-03-23 12:49:28 +0000

    no message
    git-svn-id: http://svn.neuromancy.net/magick/trunk/Magick-II@154 1d68a2a4-f605-0410-a2a0-863aa388aa54

--- include/utils.h
+++ include/utils.h
@@ -44,4 +44,48 @@ extern void wxRegisterId(long id);
 // Return the current ID
 extern long wxGetCurrentId(void);
 
+#if 0
+template <class T>
+class magick_allocator
+{
+public:
+    typedef unsigned long size_type;
+    typedef long difference_type;
+    typedef T * pointer;
+    typedef const T * const_pointer;
+    typedef T & reference;
+    typedef const T & const_reference;
+    typedef T value_type;
+    template <class U>
+	struct rebind { typedef allocator<U> other; };
+    magick_allocator();
+    template <class U>
+	magick_allocator(const magick_allocator<U>&);
+    template <class U>
+	operator=(const magick_allocator<U>&);
+    ~magick_allocator();
+    pointer address(reference r)const{return &r;};
+    const_pointer address(const_reference r)const{return &r;};
+    pointer allocate(size_type n, const_pointer hint=0);
+    void deallocate(pointer);
+    size_type max_size();
+    void construct(pointer p, const T& val);
+    void destroy(pointer p);
+};
+
+template <class T>
+magick_allocator::pointer 
+operator new(magick_allocator::size_type, magick_allocator&);
+
+template <class T, class U>
+bool 
+operator==(const magick_allocator<T>& a, 
+          const magick_allocator<U>& b);
+
+template <class T, class U>
+bool 
+operator!=(const magick_allocator<T>& a, 
+	   const magcik_allocator<U>& b) { return !(a==b);};
+#endif
+
 #endif
