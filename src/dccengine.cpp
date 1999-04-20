3b355b8d46273cc43cabfe4d8d19cb9c8672bd2c
Bill King <ungod@neuromancy.net>
1999-04-20 17:36:33 +0000

    no message
    git-svn-id: http://svn.neuromancy.net/magick/trunk/Magick-II@187 1d68a2a4-f605-0410-a2a0-863aa388aa54

--- src/dccengine.cpp
+++ src/dccengine.cpp
@@ -4,18 +4,77 @@ using namespace std;
 
 const unsigned char CTCP_DELIM_CHAR='\001';
 const unsigned char CTCP_QUOTE_CHAR='\\';
+const unsigned char CTCP_MQUOTE_CHAR='\020';
 //#define CTCP_QUOTE_EM   "\r\n\001\\"
 
 bytevector DccEngine::lowQuote(bytevector& in)
 {
     bytevector Result;
-
+    bytevector::iterator pos;
+    for(pos=in.begin();pos!=in.end();pos++)
+    {
+	if(*pos=='\0')
+	{
+	    Result.push_back(CTCP_MQUOTE_CHAR);
+	    Result.push_back('0');
+	}
+	else if(*pos=='\n')
+	{
+	    Result.push_back(CTCP_MQUOTE_CHAR);
+	    Result.push_back('n');
+	}
+	else if(*pos=='\r')
+	{
+	    Result.push_back(CTCP_MQUOTE_CHAR);
+	    Result.push_back('r');
+	}
+	else if(*pos==CTCP_MQUOTE_CHAR)
+	{
+	    Result.push_back(CTCP_MQUOTE_CHAR);
+	    Result.push_back(CTCP_MQUOTE_CHAR);
+	}
+	else
+	    Result.push_back(*pos);
+    }
     return Result;
 }
 bytevector DccEngine::lowDequote(bytevector& in)
 {
     bytevector Result;
-
+    bytevector::iterator pos;
+    for(pos=in.begin();pos!=in.end();pos++)
+    {
+	if(*pos==CTCP_MQUOTE_CHAR)
+	{
+	    if(*(pos+1)=='0')
+    	    {
+		Result.push_back('\0');
+		pos++;
+	    }
+	    else if(*(pos+1)=='n')
+    	    {
+		Result.push_back('\n');
+		pos++;
+	    }
+	    else if(*(pos+1)=='r')
+	    {
+		Result.push_back('\r');
+		pos++;
+	    }
+	    else if(*(pos+1)==CTCP_MQUOTE_CHAR)
+	    {
+		Result.push_back(CTCP_MQUOTE_CHAR);
+		pos++;
+	    }
+	    else
+	    {
+		Result.push_back(*(pos+1));
+		pos++;
+	    }
+	}
+	else
+	    Result.push_back(*pos);
+    }
     return Result;
 }
 bytevector DccEngine::ctcpQuote(bytevector& in)
@@ -45,13 +104,23 @@ bytevector DccEngine::ctcpDequote(bytevector& in)
     bytevector::iterator pos;
     for(pos=in.begin();pos!=in.end();pos++)
     {
-	if(*pos==CTCP_DELIM_CHAR&&*(pos+1)=='a')
+	if(*pos==CTCP_DELIM_CHAR)
 	{
-	    Result.push_back(CTCP_DELIM_CHAR);
-	}
-	else if(*pos==CTCP_QUOTE_CHAR&&*(pos+1)==CTCP_QUOTE_CHAR)
-	{
-	    Result.push_back(CTCP_QUOTE_CHAR);
+	    if(*(pos+1)=='a')
+	    {
+		Result.push_back(CTCP_DELIM_CHAR);
+		pos++;
+	    }
+	    else if(*(pos+1)==CTCP_QUOTE_CHAR)
+	    {
+		Result.push_back(CTCP_QUOTE_CHAR);
+		pos++;
+	    }
+	    else
+	    {
+		Result.push_back(*(pos+1));
+		pos++;
+	    }
 	}
 	else
 	    Result.push_back(*pos);
