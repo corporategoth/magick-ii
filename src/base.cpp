534d1a11302062db3fda35d9e4b4796f82c5d1a2
Bill King <ungod@neuromancy.net>
1999-07-05 02:02:51 +0000

    no message
    git-svn-id: http://svn.neuromancy.net/magick/trunk/Magick-II@273 1d68a2a4-f605-0410-a2a0-863aa388aa54

--- src/base.cpp
+++ src/base.cpp
@@ -19,10 +19,6 @@
 #include "magick.h"
 #include "log.h"
 
-#if 0
-map<unsigned long,mstring > mBase::Buffer_Tuple;
-ACE_Message_Queue<ACE_MT_SYNCH> mBase::Buffer_Queue;
-#endif
 
 Magick *mBase::Parent;
 bool mBase::TaskOpened;
@@ -47,37 +43,6 @@ void mBase::push_message(const mstring& message)
 	}
     }
     BaseTask.message(message);
-
-
-#if 0
-    static unsigned long counter=0;
-    // Braces, so that if we spawn/resume a thread, we make sure it can process
-    {
-	WLOCK lock("mBase","inputbuffer");
-	counter++;
-        {
-	    WLOCK("thread_handler", "pop_message");
-	    Buffer_Tuple[counter]=message;
-	}
-	//Buffer_Queue
-	if(Buffer_Queue.is_full())
-	{
-	    // set new water marks and spawn off a new thread.
-	    Buffer_Queue.high_water_mark(Parent->high_water_mark*(ACE_Thread_Manager::instance()->count_threads())*sizeof(unsigned long));
-	    // below is set to the same as high_water_mark so that Buffer_Queue, doesn't block on add.
-	    Buffer_Queue.low_water_mark(Buffer_Queue.high_water_mark());
-	    if(ACE_Thread_Manager::instance()->spawn(thread_handler)==-1) {
-		CP(("High water mark reached, failed to create a new thread"));
-	    } else {
-		COM(("High water mark reached, creating new thread"));
-	    }
-	}
-	ACE_Message_Block *mb=new ACE_Message_Block();
-	//todo: enqueue_prio
-	mb->copy((const char *)&counter,sizeof(unsigned long));
-	Buffer_Queue.enqueue(mb);
-    }
-#endif
 }
 
 void mBase::init(Magick *in)
@@ -94,16 +59,6 @@ void mBase::init(Magick *in)
     }
     BaseTask.message_queue_.high_water_mark(Parent->high_water_mark*sizeof(ACE_Method_Object *));
     BaseTask.message_queue_.low_water_mark(BaseTask.message_queue_.high_water_mark());
-#if 0
-    //mThread::spawn(Get_TType(),thread_handler,(void *)this);
-    //todo: setup the first high water mark and first low water mark
-    //and spawn off x threads.
-    Buffer_Queue.high_water_mark(Parent->high_water_mark*sizeof(unsigned long));
-    // below is set to the same as high_water_mark so that Buffer_Queue, doesn't block on add.
-    Buffer_Queue.low_water_mark(Buffer_Queue.high_water_mark());
-    if(ACE_Thread_Manager::instance()->spawn(thread_handler)==-1)
-	CP(("Failed to create initial thread"));
-#endif
 }
 
 void mBase::send_cmd(const mstring & source, const mstring & fmt, ...)
@@ -131,99 +86,6 @@ void mBase::shutdown()
     for(int i=0;i<j;i++)
 	BaseTask.shutdown();
 }
-
-#if 0
-void *mBase::thread_handler(void *owner)
-{
-    NFT("thread_handler");
-    //todo ugh, major revamp Owner will be unknown till we figure out who
-    // the thread is to go to (via the message)
-    // we then execute the message, finally we (locked) check if we are below
-    // low water mark, if so, then we return.
-    // if Owner->MSG() == false then we toss message, else we execute it.
-    while(Parent->shutdown()==false)
-    {
-	COM(("Attempting to read another thread ..."));
-        ACE_Message_Block *mb;
-	mBase::Buffer_Queue.dequeue(mb);
-	if(mb!=NULL)
-	{
-	    unsigned long messageid=*(mb->rd_ptr());
-	    delete mb; // as it's no longer needed once we get the message id.
-	    mstring message;
-	    {
-		WLOCK("thread_handler", "pop_message");
-		message=mBase::Buffer_Tuple[messageid];
-		mBase::Buffer_Tuple.erase(messageid);
-	    }
-
-	    mstring tmp[2];
-	    if(message[0u]==':')
-	    {
-		// with from prefix
-		tmp[0]=message.ExtractWord(2,": ").UpperCase();
-		tmp[1]=message.ExtractWord(3,": ");
-	    }
-	    else
-	    {
-		// no from prefix
-		tmp[0]=message.ExtractWord(1,": ").UpperCase();
-		tmp[1]=message.ExtractWord(2,": ");
-	    }
-	    
-	    // check if on ignore list and throw to the "ignore logging service" if log ignored user commands is on.
-	    // maybe we should have a hit count for logging? x ignores in x minutes = start logging that sucker. 
-
-	    if (tmp[0] == "PRIVMSG" || tmp[0] == "NOTICE") {
-		mstring names;
-
-		// Find out if the target nick is one of the services 'clones'
-		// (and if it is, which one?)  Pass the message to them if so.
-//		     if ((names=" "+OperServ::getnames()+" ").Find(" "+tmp[1]+" "))
-//		    Parent->operserv.execute(message);
-		     if ((names=" "+Parent->nickserv.getnames()+" ").Find(" "+tmp[1]+" "))
-		    Parent->nickserv.execute(message);
-		else if ((names=" "+Parent->chanserv.getnames()+" ").Find(" "+tmp[1]+" "))
-		    Parent->chanserv.execute(message);
-//		else if ((names=" "+MemoServ::getnames()+" ").Find(" "+tmp[1]+" "))
-//		    Parent->memoserv.execute(message);
-//		else if ((names=" "+HelpServ::getnames()+" ").Find(" "+tmp[1]+" "))
-//		    Parent->helpserv.execute(message);
-
-		// How do we want to handle custom services (BOB created)?
-
-		// Not a MSG/NOTICE to services, fall through
-		// (it could be to a channel, or unrecognised nick).
-		else
-		    Parent->Server.execute (message);
-
-	    } else {
-		// This handles all non-msgs/notices.
-		Parent->Server.execute (message);
-	    }
-
-	    /* Locking here because we dont want two threads
-	     * dieing because they both counted at the same
-	     * time and before the other had died.  Unlikely,
-	     * but it CAN happen.
-	     */
-	    {
-		MLOCK("thread_handler", "LWM");
-		if(mBase::Buffer_Queue.message_count()<
-			Parent->high_water_mark*(ACE_Thread_Manager::instance()->count_threads()-2)*sizeof(unsigned long)+Parent->low_water_mark*sizeof(unsigned long))
-		{
-		    mBase::Buffer_Queue.high_water_mark(Parent->high_water_mark*(ACE_Thread_Manager::instance()->count_threads()-1)*sizeof(unsigned long));
-		    mBase::Buffer_Queue.low_water_mark(mBase::Buffer_Queue.high_water_mark());
-		    COM(("Low water mark reached, killing thread."));
-		    return NULL;
-		}
-	    }
-	}
-    }
-    CP(("Shutdown code received, killing thread."));
-    return NULL;
-}
-#endif
 
 NetworkServ::NetworkServ(Magick *in_Parent) : mBase(in_Parent)
 {
