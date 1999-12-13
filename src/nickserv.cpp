464141f2ccaa898fdac1d2a1052c16938aa78d55
Preston A. Elder <prez@neuromancy.net>
1999-12-13 10:31:39 +0000

    *** empty log message ***
    git-svn-id: http://svn.neuromancy.net/magick/trunk/Magick-II@479 1d68a2a4-f605-0410-a2a0-863aa388aa54

--- src/nickserv.cpp
+++ src/nickserv.cpp
@@ -276,14 +276,14 @@ void Nick_Live_t::InFlight_t::End(unsigned long filenum)
 			    Parent->chanserv.stored[recipiant.LowerCase()].Founder() + ").");
 			if (Parent->chanserv.IsLive(recipiant))
 			{
-			    Chan_Live_t *chan = Parent->chanserv.live[recipiant.LowerCase()];
+			    Chan_Live_t *chan = &Parent->chanserv.live[recipiant.LowerCase()];
 			    int i;
 			    for(i=0; i<chan->Users(); i++)
 			    {
 				if (Parent->chanserv.stored[recipiant.LowerCase()].GetAccess(chan->User(i), "READMEMO"))
 				{
 				    send(service, nick, "There is a new channel news article (#" +
-					itoa(Parent->memoserv.channel[recipiant.LowerCase()].size()) + ").");
+					mstring(itoa(Parent->memoserv.channel[recipiant.LowerCase()].size())) + ").");
 				}
 			    }
 			}
@@ -304,7 +304,7 @@ void Nick_Live_t::InFlight_t::End(unsigned long filenum)
 			    send(service, nick, "Memo has been sent to " + recipiant + " (" + realrecipiant + ").");
 			    if (Parent->nickserv.stored[realrecipiant.LowerCase()].IsOnline())
 				send(service, nick, "You have a new memo from " + sender + " (#" +
-					itoa(Parent->memoserv.nick[realrecipiant.LowerCase()].size()) + ").")
+					mstring(itoa(Parent->memoserv.nick[realrecipiant.LowerCase()].size())) + ").");
 			}
 			else if (File())
 			{
