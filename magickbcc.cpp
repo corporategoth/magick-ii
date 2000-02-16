22a9a605d61167b641d08a5349e935db468acc76
Bill King <ungod@neuromancy.net>
2000-02-16 09:22:29 +0000

    initial checkin: fixing for borland compilability
    git-svn-id: http://svn.neuromancy.net/magick/trunk/Magick-II@591 1d68a2a4-f605-0410-a2a0-863aa388aa54

---
+++ magickbcc.cpp
@@ -0,0 +1,32 @@
+#if defined(__BORLANDC__)
+#include <condefs.h>
+USEUNIT(".\src\base.cpp");
+USEUNIT(".\src\chanserv.cpp");
+USEUNIT(".\src\commserv.cpp");
+USEUNIT(".\src\confbase.cpp");
+USEUNIT(".\src\cryptstream.cpp");
+USEUNIT(".\src\datetime.cpp");
+USEUNIT(".\src\dccengine.cpp");
+USEUNIT(".\src\EscLexer.cpp");
+USEUNIT(".\src\EscParser.cpp");
+USEUNIT(".\src\fileconf.cpp");
+USEUNIT(".\src\ircsocket.cpp");
+USEUNIT(".\src\lockable.cpp");
+USEUNIT(".\src\log.cpp");
+USEUNIT(".\src\magick.cpp");
+USEUNIT(".\src\main.cpp");
+USEUNIT(".\src\memoserv.cpp");
+USEUNIT(".\src\mstream.cpp");
+USEUNIT(".\src\mstring.cpp");
+USEUNIT(".\src\nickserv.cpp");
+USEUNIT(".\src\operserv.cpp");
+USEUNIT(".\src\server.cpp");
+USEUNIT(".\src\servmsg.cpp");
+USEUNIT(".\src\textfile.cpp");
+USEUNIT(".\src\trace.cpp");
+USEUNIT(".\src\utils.cpp");
+USEUNIT(".\src\variant.cpp");
+USEFILE("vctobpr.log");
+#endif // defined(__BORLANDC__)
+
+#define WinMain
