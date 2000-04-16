c6133fba8e8419f998e2102d8d0fef9bd70057c4
Preston A. Elder <prez@neuromancy.net>
2000-04-16 06:18:22 +0000

    Started adding body to the documentation...
    git-svn-id: http://svn.neuromancy.net/magick/trunk/Magick-II@662 1d68a2a4-f605-0410-a2a0-863aa388aa54

---
+++ docs/docs.cpp
@@ -0,0 +1,37 @@
+//---------------------------------------------------------------------------
+#include <vcl.h>
+#pragma hdrstop
+USEFILE("magick.ini");
+USEFILE("..\Install");
+USEFILE("..\ChangeLog");
+USEFILE("..\Copying");
+USEFILE("..\Authors");
+USEFILE("..\News");
+USEFILE("..\Readme");
+USEFILE("devel\communication.txt");
+USEFILE("devel\config.h");
+USEFILE("devel\dbase.txt");
+USEFILE("devel\develop.txt");
+USEFILE("devel\policy.txt");
+USEFILE("devel\requirements.txt");
+USEFILE("devel\suggest.txt");
+USEFILE("devel\versionstamps.txt");
+USEFILE("rfcs\The Client-To-Client Protocol (CTCP).txt");
+USEFILE("rfcs\rfc854.txt");
+USEFILE("rfcs\rfc855.txt");
+USEFILE("rfcs\rfc856.txt");
+USEFILE("rfcs\rfc857.txt");
+USEFILE("rfcs\rfc858.txt");
+USEFILE("rfcs\rfc859.txt");
+USEFILE("rfcs\rfc860.txt");
+USEFILE("rfcs\rfc861.txt");
+USEFILE("rfcs\rfc959.txt");
+USEFILE("rfcs\rfc1459.txt");
+USEFILE("text\install.txt");
+USEFILE("text\formats.txt");
+USEFILE("text\commands.txt");
+//---------------------------------------------------------------------------
+#define Library
+
+// To add a file to the library use the Project menu 'Add to Project'.
+
