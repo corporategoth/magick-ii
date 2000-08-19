e19dd0ec0b994160b95a1dab07a9f4e9cbe7e36e
Bill King <ungod@neuromancy.net>
2000-08-19 15:58:54 +0000

    no message
    git-svn-id: http://svn.neuromancy.net/magick/trunk/Magick-II@808 1d68a2a4-f605-0410-a2a0-863aa388aa54

---
+++ magick_keygenbcc.cpp
@@ -0,0 +1,9 @@
+#include <condefs.h>
+//---------------------------------------------------------------------------
+USEUNIT("src\magick_keygen.c");
+USELIB("src\des\crypto.lib");
+//---------------------------------------------------------------------------
+#define main dummymain
+int main(int argc, char **argv)
+{
+}
