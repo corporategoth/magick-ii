f4434b07ea911e71be5610d6994c7b1d4a45daa0
Preston A. Elder <prez@neuromancy.net>
2001-11-07 00:21:10 +0000

    Added dos running batch files (to force the stamp ...)
    git-svn-id: http://svn.neuromancy.net/magick/trunk/Magick-II@1053 1d68a2a4-f605-0410-a2a0-863aa388aa54

---
+++ helper/run_magick.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+IF NOT EXIST need_stamp.sem GOTO START_MAGICK
+ECHO Stamping new random encryption keys on Magick binaries ...
+magick_stampkeys.exe magick.exe magick_keygen.exe
+del need_stamp.sem
+:START_MAGICK
+magick.exe %*
+
