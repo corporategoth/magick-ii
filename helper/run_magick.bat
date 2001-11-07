7baf7f1cce0eff68cada9a13f64178eb2e1e9375
Preston A. Elder <prez@neuromancy.net>
2001-11-07 00:51:18 +0000

    Re-vamped batch files
    git-svn-id: http://svn.neuromancy.net/magick/trunk/Magick-II@1054 1d68a2a4-f605-0410-a2a0-863aa388aa54

--- helper/run_magick.bat
+++ helper/run_magick.bat
@@ -4,5 +4,9 @@ ECHO Stamping new random encryption keys on Magick binaries ...
 magick_stampkeys.exe magick.exe magick_keygen.exe
 del need_stamp.sem
 :START_MAGICK
+ECHO Launching Magick IRC Services ...
+ECHO.
 magick.exe %*
-
+ECHO.
+ECHO Executed has ended.
+pause
--- helper/run_magick_keygen.bat
+++ helper/run_magick_keygen.bat
@@ -4,5 +4,10 @@ ECHO Stamping new random encryption keys on Magick binaries ...
 magick_stampkeys.exe magick.exe magick_keygen.exe
 del need_stamp.sem
 :START_MAGICK
+ECHO Launching Magick Key Generator ...
+ECHO.
 magick_keygen.exe %*
+ECHO.
+ECHO Execution has ended.
+pause
 
