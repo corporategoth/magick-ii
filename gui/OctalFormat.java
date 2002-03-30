578a06f6b5c7a93547cfd44f2e5fda5f682e3d70
Preston A. Elder <prez@neuromancy.net>
2002-03-30 08:35:28 +0000

    Added standardized headers to all files, and removed imports that were redundant or not needed.
    git-svn-id: http://svn.neuromancy.net/magick/trunk/Magick-II@1206 1d68a2a4-f605-0410-a2a0-863aa388aa54

--- gui/OctalFormat.java
+++ gui/OctalFormat.java
@@ -1,5 +1,29 @@
+// Magick IRC Services
 //
+// (c) 1997-2002 Preston Elder <prez@magick.tm>
+// (c) 1998-2002 William King <ungod@magick.tm>
 //
+// The above copywright may not be removed under any circumstances,
+// however it may be added to if any modifications are made to this
+// file.  All modified code must be clearly documented and labelled.
+//
+// This code is released under the GNU General Public License, which
+// means (in short), it may be distributed freely, and may not be sold
+// or used as part of any closed-source product.  Please check the
+// COPYING file for full rights and restrictions of this software.
+//
+// =======================================================================
+// rcsid = @(#)$Id$
+// =======================================================================
+//
+// For official changes (by the Magick Development Team),please
+// check the ChangeLog* files that come with this distribution.
+//
+// Third Party Changes (please include e-mail address):
+//
+// N/A
+//
+// =======================================================================
 
 import javax.swing.*;
 import java.text.*;
