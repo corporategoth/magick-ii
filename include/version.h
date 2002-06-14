c1217348ad15ab5b3e122c1098bfc87b7d8d9091
Preston A. Elder <prez@neuromancy.net>
2002-06-14 14:19:13 +0000

    Updated version to b13
    git-svn-id: http://svn.neuromancy.net/magick/trunk/Magick-II@1235 1d68a2a4-f605-0410-a2a0-863aa388aa54

--- include/version.h
+++ include/version.h
@@ -71,7 +71,7 @@ const mstring SLOGAN = "Power to the PEOPLE!";
 const mstring EMAIL = "general@magick.tm";
 const mstring HOMEPAGE = "http://www.magick.tm";
 const mstring DOWNLOAD = "ftp://ftp.magick.tm/pub/magick.tm/Magick-II/beta";
-const mstring RELEASE = "b12";
+const mstring RELEASE = "b13";
 const mstring PATCH1 = "";
 const mstring PATCH2 = "";
 const mstring PATCH3 = "";
@@ -83,35 +83,35 @@ const mstring PATCH8 = "";
 const mstring PATCH9 = "";
 
 // This gets sent with /msg HelpServ CREDITS
-const mstring credits[] =
-{
-FULLNAME + " - " + SLOGAN, "  (c) 1997-2002 Preston Elder <prez@magick.tm>",
-	"  (c) 1998-2002 William King <ungod@magick.tm>", "",
-	mstring(PACKAGE) + " is dedicated to the memory of PreZ's beloved",
-	"sister, Stacey Louise Elder (Jan 1975 - Feb 1998) and",
-	"Ungod's brother Edward Kevin King (Aug 1982 - Nov 1997).", "",
-	"The " + mstring(PACKAGE) + " home page can be found at:", "    " + HOMEPAGE + "", "",
-	mstring(PACKAGE) + " is freely downloadable from:", "    " + DOWNLOAD + "", "",
-	"The " + mstring(PACKAGE) + " public mailing list is " + EMAIL + ".",
-	"To subscribe, email majordomo@magick.tm with 'subscribe",
-	mstring(PACKAGE) + "' (no quotes) in the text of the message.", "",
-	"Please type /MSG %s CONTRIB for full credits (flood).", "---EOM---"};
+const mstring credits[] = {
+    FULLNAME + " - " + SLOGAN, "  (c) 1997-2002 Preston Elder <prez@magick.tm>",
+    "  (c) 1998-2002 William King <ungod@magick.tm>", "",
+    mstring(PACKAGE) + " is dedicated to the memory of PreZ's beloved",
+    "sister, Stacey Louise Elder (Jan 1975 - Feb 1998) and",
+    "Ungod's brother Edward Kevin King (Aug 1982 - Nov 1997).", "",
+    "The " + mstring(PACKAGE) + " home page can be found at:", "    " + HOMEPAGE + "", "",
+    mstring(PACKAGE) + " is freely downloadable from:", "    " + DOWNLOAD + "", "",
+    "The " + mstring(PACKAGE) + " public mailing list is " + EMAIL + ".",
+    "To subscribe, email majordomo@magick.tm with 'subscribe",
+    mstring(PACKAGE) + "' (no quotes) in the text of the message.", "",
+    "Please type /MSG %s CONTRIB for full credits (flood).", "---EOM---"
+};
 
 // This gets sent with /msg HelpServ CONTRIB
 // or a /INFO request on the magick server.
-const mstring contrib[] =
-{
-FULLNAME + " - " + SLOGAN, "", "Programmers: ", "    PreZ           Preston A. Elder        prez@magick.tm",
-	"    Ungod          William King            ungod@magick.tm", "", "Design Team: ",
-	"    Lord_Striker   Patrick Prins           striker@magick.tm", "", "Special Thanks To:",
-	"    Coca Cola Amatil     Life support ... *bleep, bleep*",
-	"    Nescafe Blend 43     See above (really, any kind of caffeine!)",
-	"    Tschaicovski         If you don't understand ... you never will.", "", "Also Thanks To:",
-	"    Kelly Hill, Emma Ellison, Marieke Bermon, Matt Heckaman", "", "Library Credits: ",
-	"    ACE            http://www.cs.wustl.edu/~schmidt/ACE.html",
-	"    cint           http://root.cern.ch/root/Cint.html",
-	"    openssl        http://www.openssl.org (blowfish + md5)",
-	"    expat          http://www.jclark.com/xml/expat.html",
-	"    sxp            http://www.sourceforge.net/project/?group_id=2280", "---EOM---"};
+const mstring contrib[] = {
+    FULLNAME + " - " + SLOGAN, "", "Programmers: ", "    PreZ           Preston A. Elder        prez@magick.tm",
+    "    Ungod          William King            ungod@magick.tm", "", "Design Team: ",
+    "    Lord_Striker   Patrick Prins           striker@magick.tm", "", "Special Thanks To:",
+    "    Coca Cola Amatil     Life support ... *bleep, bleep*",
+    "    Nescafe Blend 43     See above (really, any kind of caffeine!)",
+    "    Tschaicovski         If you don't understand ... you never will.", "", "Also Thanks To:",
+    "    Kelly Hill, Emma Ellison, Marieke Bermon, Matt Heckaman", "", "Library Credits: ",
+    "    ACE            http://www.cs.wustl.edu/~schmidt/ACE.html",
+    "    cint           http://root.cern.ch/root/Cint.html",
+    "    openssl        http://www.openssl.org (blowfish + md5)",
+    "    expat          http://www.jclark.com/xml/expat.html",
+    "    sxp            http://www.sourceforge.net/project/?group_id=2280", "---EOM---"
+};
 
 #endif
