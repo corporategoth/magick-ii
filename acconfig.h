cde3195678ea1487e11fad9bdbd35e75dd7b6ec7
Preston A. Elder <prez@neuromancy.net>
2002-10-04 17:11:10 +0000

    Added more STL fixing stuff ... ugh, damn M$ STL.
    git-svn-id: http://svn.neuromancy.net/magick/trunk/Magick-II@1450 1d68a2a4-f605-0410-a2a0-863aa388aa54

--- acconfig.h
+++ acconfig.h
@@ -43,9 +43,19 @@
 #undef GETPASS
 #endif
 
-/* Do we need to use mem_fun1_ref when we want a binary_function */
-#ifndef HAVE_MEM_FUN1_REF
-#undef HAVE_MEM_FUN1_REF
+/* Can we use 'return f()' where f has a void return type. */
+#ifndef HAVE_RETURN_VOID
+#undef HAVE_RETURN_VOID
+#endif
+
+/* Do we need versions of mem_fun (and friends) that accept const arguments */
+#ifndef HAVE_MEM_FUN_CONST
+#undef HAVE_MEM_FUN_CONST
+#endif
+
+/* Do we need to use mem_fun1 (and mem_fun1_ref) when we want a binary_function */
+#ifndef HAVE_MEM_FUN1
+#undef HAVE_MEM_FUN1
 #endif
 
 /* Crypto key 1 placeholder */
