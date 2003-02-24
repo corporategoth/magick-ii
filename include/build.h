627a3d2a12b67394cc4d947f29010dba3fa4082d
Preston A. Elder <prez@neuromancy.net>
2003-02-24 00:45:48 +0000

    Added build header file
    git-svn-id: http://svn.neuromancy.net/magick/trunk/Magick-II@1572 1d68a2a4-f605-0410-a2a0-863aa388aa54

---
+++ include/build.h
@@ -0,0 +1,44 @@
+#ifndef WIN32
+  #pragma interface
+#endif
+/* Magick IRC Services
+**
+** (c) 1997-2003 Preston Elder <prez@magick.tm>
+** (c) 1998-2003 William King <ungod@magick.tm>
+**
+** The above copywright may not be removed under any circumstances,
+** however it may be added to if any modifications are made to this
+** file.  All modified code must be clearly documented and labelled.
+**
+** This code is released under the Artistic License v2.0 or better.
+** The full text of this license should be contained in a file called
+** COPYING distributed with this code.  If this file does not exist,
+** it may be viewed here: http://www.magick.tm/m2/license.html
+**
+** ======================================================================= */
+#ifndef _BUILD_H
+#define _BUILD_H
+#include "pch.h"
+RCSID(version_h, "@(#) $Id$");
+
+/* ======================================================================= **
+**
+** For official changes (by the Magick Development Team),please
+** check the ChangeLog* files that come with this distribution.
+**
+** Third Party Changes (please include e-mail address):
+**
+** N/A
+**
+** ======================================================================= */
+
+#ifdef _MSC_VER
+  #pragma warning(disable:4786)
+#endif
+
+#include "mstring.h"
+
+const mstring BUILD_TIME = __DATE__ " " __TIME__;
+const int BUILD_NUMBER = 0;
+
+#endif
